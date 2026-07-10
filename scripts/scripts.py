f894c1215855ba9988f97cb391eaea0401cfefe0
TYS <551835@mail.muni.cz>
2026-07-10 12:52:03 +0200

    feat: Add data configuartion, add illustration.ipynb, add /scripts, change population.py to dynamic.py

---
+++ scripts/scripts.py
@@ -0,0 +1,13 @@
+
+import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(config_path="../conf",config_name='config',version_base=None)
+def main(cfg:DictConfig):
+    print(cfg)
+    
+    
+    
+if __name__ == "__main__":
+    main()
